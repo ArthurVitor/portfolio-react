2664467396e98bde24ae9019a8c5b8a380acc81e
ArthurVitor <arthurvitorgn@gmail.com>
2025-02-11 15:54:09 -0300

    feat: routing, folder structure, initial components

---
+++ src/components/NavBar/NavBarComponent.tsx
@@ -0,0 +1,9 @@
+function NavBarComponent() {
+    return (
+        <>
+            <h1>nav bar</h1>
+        </>
+    )
+}
+
+export { NavBarComponent }
---
+++ src/pages/LayoutPage/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import { NavBarComponent } from "../../components/NavBar/NavBarComponent";
+import './Layout.css';
+
+export function Layout() {
+    return (
+        <>
+            <NavBarComponent />
+
+            <Outlet />
+        </>
+    )
+}
